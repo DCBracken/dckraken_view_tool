1cb513b0407d25d541d1abad0e1a5ea744e2b426
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 21:19:23 +0000

    dev tool initial commit

---
+++ lib/dckraken_view_tool.rb
@@ -0,0 +1,6 @@
+require "dckraken_view_tool/version"
+require "dckraken_view_tool/renderer"
+
+module DckrakenViewTool
+  # Your code goes here...
+end
---
+++ lib/dckraken_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DckrakenViewTool
+  VERSION = "0.1.0"
+end
